a5fa67738643d6635909947e09e0e93d41b7d45c
Hibram007 <87105978+Hibram007@users.noreply.github.com>
2021-12-05 17:38:51 -0700

    validation added

--- controllers/pizza-controller.js
+++ controllers/pizza-controller.js
@@ -39,9 +39,9 @@ const pizzaController = {
       .catch(err => res.json(err));
   },
 
-  // update pizza by id
+  // update pizza by id -- ruValidators needs to be manually set to validate data.
   updatePizza({ params, body }, res) {
-    Pizza.findOneAndUpdate({ _id: params.id }, body, { new: true })
+    Pizza.findOneAndUpdate({ _id: params.id }, body, { new: true,runValidators: true })
       .then(dbPizzaData => {
         if (!dbPizzaData) {
           res.status(404).json({ message: 'No pizza found with this id!' });
